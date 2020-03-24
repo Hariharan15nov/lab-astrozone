3baf189718d028f067f567dd2bd185a73c090706
Hariharan15nov <hariharan111999@gmail.com>
2020-03-24 15:33:17 +0530

    Update

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,15 @@
 package model;
-
+import java.util.Date;
 //Type your code here
+class User{
+	String name;
+	String email;
+	LocalDate dob;
+	String gender;
+	User(String name,String email,LocalDate dob,String gender){
+		this.name=name;
+		this.email=email;
+		this.dob=dob;
+		this.gender=gender;
+	}
+}
